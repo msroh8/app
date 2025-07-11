45599ae2312e84ed0a0523cf7cbbd927e244717a
msroh8 <msroh8@gmail.com>
2025-07-11 12:03:38 +0300

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = "school-hr-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// تثبيت الخدمة وتخزين الملفات
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// استبدال الطلبات من الكاش إذا كان متوفر
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// تحديث الكاش إذا تغير الإصدار
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
 
